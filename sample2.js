bf02ef0607821d6f96d2f52b11bf72c9e063c8f9
Gokulakannan01 <105979886+Gokulakannan01@users.noreply.github.com>
2022-06-15 23:05:02 +0530

    Add files via upload

---
+++ sample2.js
@@ -0,0 +1,29 @@
+let karthi = {
+    name: "karthi",
+    deept: "mech",
+    college: "Anna University",
+    marks:  [100,100,100,100,100],
+    totalmarks: function () {
+    sum = 0;
+    for (let i = 0; i < this.marks.length; i++) {
+        sum += this.marks[i];
+    }
+    console.log(sum);
+    },
+};
+karthi.totalmarks();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
